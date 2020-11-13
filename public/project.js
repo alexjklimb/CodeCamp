921d2d13f73a7523e797e37d555ef7226f229791
Slade <admin@admins-MacBook-Pro.local>
2020-11-13 13:30:44 -0700

    I made registration

--- public/project.js
+++ public/project.js
@@ -1,14 +1,15 @@
+
 var userBox = document.querySelector("#user-name");
-var passwordBox = document.querySelector("#password-name");
+var passwordBox = document.querySelector("#password");
 var submitButton = document.querySelector("#submit");
 
 submitButton.onclick = function() {
-    var data = userBox.value;
-    data += passwordBox.value;
-    data += submitButton.value;
+    var data = "username=" + encodeURIComponent(userBox.value);
+    data += "&password=" + encodeURIComponent(passwordBox.value);
+    console.log(data)
     fetch("http://localhost:8080/users", {
     method: 'POST',
-    credentials: 'include',
+    // credentials: 'include',
     body: data,
     headers: {
         'Content-Type': 'application/x-www-form-urlencoded'
